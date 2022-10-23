2801a0ea0ec992a056768065195fb04080f51b4f
Vincent <vincent@mahn.ke>
2022-10-23 02:28:31 +0200

    feat: Added graph chore: Added dark mode

--- crawl.js
+++ crawl.js
@@ -4,8 +4,8 @@ const itemsWithNutritions = a.filter(e=>e.nutritions.length > 1 && e.nutritions.
 const sortedItemsWithNutritions = itemsWithNutritions.sort((a,b)=> {
     const proteinA = parseFloat(a.nutritions.find(c=>c.includes("Eiweiß:")).replace(/`./, "").replace(",", ".").replace(/\D*/, ""));
     const proteinB = parseFloat(b.nutritions.find(c=>c.includes("Eiweiß:")).replace(/`./, "").replace(",", ".").replace(/\D*/, ""));
-    const fatA = parseFloat(a.nutritions.find(c=>c.includes("Fett:")).replace(/`./, "").replace(",", ".").replace(/\D*/, ""));
-    const fatB = parseFloat(b.nutritions.find(c=>c.includes("Fett:")).replace(/`./, "").replace(",", ".").replace(/\D*/, ""));
+    const fatA = parseFloat(a.nutritions.find(c=>c.includes("Fett:")).replace(/`./, "").replace(",", ".").replace(/\D*/, "")) * 2;
+    const fatB = parseFloat(b.nutritions.find(c=>c.includes("Fett:")).replace(/`./, "").replace(",", ".").replace(/\D*/, "")) * 2;
     const carbsA = parseFloat(a.nutritions.find(c=>c.includes("Kohlenhydrate:")).replace(/`./, "").replace(",", ".").replace(/\D*/, ""));
     const carbsB = parseFloat(b.nutritions.find(c=>c.includes("Kohlenhydrate:")).replace(/`./, "").replace(",", ".").replace(/\D*/, ""));
     a.rank = (proteinA/(fatA+carbsA));
@@ -13,8 +13,27 @@ const sortedItemsWithNutritions = itemsWithNutritions.sort((a,b)=> {
     return b.rank - a.rank;
 }).filter(c=>!Number.isNaN(c.rank)).sort((a,b)=>b.rank-a.rank);
 
-console.log(a.length);
-console.log(itemsWithNutritions.length);
-console.log(itemsWithNutritions[0])
+const generateItem = (item) => {
+    let output = "";
 
-debugger;
+    const nutritions = {
+        carbs: parseFloat(item.nutritions.find(c=>c.includes("Kohlenhydrate:")).replace(/`./, "").replace(",", ".").replace(/\D*/, "")),
+        fat: parseFloat(item.nutritions.find(c=>c.includes("Fett:")).replace(/`./, "").replace(",", ".").replace(/\D*/, "")) * 2,
+        protein: parseFloat(item.nutritions.find(c=>c.includes("Eiweiß:")).replace(/`./, "").replace(",", ".").replace(/\D*/, ""))
+    };
+
+    nutritions.score = (nutritions.protein/(nutritions.fat+nutritions.carbs));
+
+    output += `<div class="product" data-score="${nutritions.score}" data-carbs="${nutritions.carbs}" data-fat="${nutritions.fat}" data-protein="${nutritions.protein}">`;
+    if (item.images.length > 0)
+    {
+        output += `<img src="${item.images[0]}">`;
+    }
+    output += `<p>${item.name}</p>`
+    output += `<div class="canvas-container"> <canvas width="200" height="200" style="width:200px; height:200px"></canvas></div>`;
+    output += "</div>";
+
+    return output;
+};
+
+fs.writeFileSync("output.html", fs.readFileSync("layout.html").toString().replace("{{content}}", sortedItemsWithNutritions.map(generateItem).join("\r\n")));
